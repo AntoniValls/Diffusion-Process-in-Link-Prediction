ea9f27991d1eb7a5c829b1f31068d3d437b87324
Antoni Valls <101109878+AntoniValls@users.noreply.github.com>
2024-08-05 03:41:04 +0200

    fit lognormal and centrality measures

---
+++ diffusion/all_centralities.py
@@ -0,0 +1,158 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import cmocean
+import torch_geometric as tg
+import numpy as np
+from utils.data_utils import data_loader
+from diffusion.jackson_metrics import decay_centrality, diffusion_centrality, Godfather
+from diffusion.complex_diffusion import get_complex_path, paralell_complex_path
+import os
+import json
+
+class CentralityMeasures:
+    '''
+    This object computes the centrality measures of any graph from the torch_geometric library
+    '''
+    def __init__(self, tgm_type, name, p, T, threshold, transform=None):
+
+        self.tgm_type = tgm_type
+        self.name = name
+        self.transform = transform
+        self.p = p
+        self.T = T
+        self.threshold = threshold
+
+        # Obtaing the graph object
+        self.dataset = data_loader(self.tgm_type, self.name, self.transform)
+        self.G = tg.utils.to_networkx(self.dataset[0])
+        
+        self.degree_centrality = None
+        self.betweenness_centrality = None
+        self.decay_centrality = None
+        self.diffusion_centrality = None
+        self.godfather = None
+        self.complex_path = None
+        self.parallel_complex_path = None
+
+    def calculate_degree_centrality(self):
+        self.degree_centrality = nx.degree_centrality(self.G)
+        return self.degree_centrality
+        
+    def calculate_betweenness_centrality(self):
+        self.betweenness_centrality = nx.betweenness_centrality(self.G)
+        return self.betweenness_centrality
+        
+    def calculate_decay_centrality(self):
+        self.decay_centrality = decay_centrality(self.G, self.p, self.T)
+        return self.decay_centrality
+        
+    def calculate_diffusion_centrality(self):
+        self.diffusion_centrality = diffusion_centrality(self.G, self.T)
+        return self.diffusion_centrality
+        
+    def calculate_godfather(self):
+        self.godfather = Godfather(self.G)
+        return self.godfather
+        
+    def calculate_complex_path(self):
+        self.complex_path = get_complex_path(self.G, self.threshold)
+        return self.complex_path
+        
+    def calculate_parallel_complex_path(self):
+        self.parallel_complex_path = paralell_complex_path(self.G, self.threshold)
+        return self.parallel_complex_path
+        
+    def visualize_centralities(self):
+        '''
+        Visualitzation of each centrality by coloring the nodes.
+        Not feasible for big graphs
+        '''
+        fig, axes = plt.subplots(4, 2, figsize=(15, 20))
+        axes = axes.flatten()
+        
+        centrality_measures = [
+            ('Degree Centrality', self.degree_centrality),
+            ('Betweenness Centrality', self.betweenness_centrality),
+            ('Decay Centrality', self.decay_centrality),
+            ('Diffusion Centrality', self.diffusion_centrality),
+            ('Godfather Centrality', self.godfather),
+            ('Complex Path Centrality', self.complex_path),
+            ('Parallel Complex Path Centrality', self.parallel_complex_path)
+        ]
+        
+        # Define a fixed layout
+        pos = nx.spring_layout(self.G, seed=42)
+        
+        for ax, (title, centrality) in zip(axes, centrality_measures):
+            if centrality is not None:
+                nx.draw(self.G, pos=pos, with_labels=True, node_color=list(centrality.values()), ax=ax, node_size=500, cmap=cmocean.cm.thermal)
+                ax.set_title(title)
+            else:
+                ax.set_title(f"{title} (Not Calculated)")
+                ax.axis('off')
+        
+        plt.tight_layout()
+        plt.show()
+
+    def compare_centralities(self):
+        '''
+        Greedy "comparision"
+        '''
+        measures = {
+            'Degree Centrality': self.degree_centrality,
+            'Betweenness Centrality': self.betweenness_centrality,
+            'Decay Centrality': self.decay_centrality,
+            'Diffusion Centrality': self.diffusion_centrality,
+            'Godfather Centrality': self.godfather,
+            'Complex Path Centrality': self.complex_path,
+            'Parallel Complex Path Centrality': self.parallel_complex_path
+        }
+        
+        for measure_name, centrality in measures.items():
+            if centrality is not None:
+                plt.figure(figsize=(10, 5))
+                plt.bar(centrality.keys(), centrality.values(), color='b', alpha=0.7)
+                plt.xlabel('Nodes')
+                plt.ylabel('Centrality Value')
+                plt.title(measure_name)
+                plt.show()
+                
+    def save_centralities(self):
+        '''
+        Function that saves the mesures inside the data folder
+        '''
+        
+        measures = {
+            'degree_centrality': self.degree_centrality,
+            'betweenness_centrality': self.betweenness_centrality,
+            'decay_centrality': self.decay_centrality,
+            'diffusion_centrality': self.diffusion_centrality,
+            'godfather_centrality': self.godfather,
+            'complex_path_centrality': self.complex_path,
+            'parallel_complex_path_centrality': self.parallel_complex_path
+        }
+        
+        directory = f"data/{self.tgm_type}/{self.name}/measures"
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        
+        for measure_name, centrality in measures.items():
+            if centrality is not None:
+                file_path = os.path.join(directory, f"{measure_name}.json")
+                with open(file_path, 'w') as f:
+                    json.dump(centrality, f)
+
+def run_all_centralities(tgm_type, name, p, T, threshold, transform=None):
+    '''
+    Run all the measures
+    '''
+    cm = CentralityMeasures(tgm_type, name, p, T, threshold, transform=None)
+    cm.calculate_degree_centrality()
+    cm.calculate_betweenness_centrality()
+    cm.calculate_decay_centrality()
+    cm.calculate_diffusion_centrality()
+    # cm.calculate_godfather() #correct Godfather to make it faster
+    # cm.calculate_complex_path()
+    cm.calculate_parallel_complex_path()
+    cm.save_centralities()
+    return
--- diffusion/jackson_metrics.py
+++ diffusion/jackson_metrics.py
@@ -1,5 +1,6 @@
 import numpy as np
 import networkx as nx
+from tqdm import tqdm
 
 '''
 Script about the centrality mesures from Jackson et. al. 2020
@@ -8,7 +9,7 @@ Script about the centrality mesures from Jackson et. al. 2020
 def decay_centrality(G, p, T):
 
     centrality = {} 
-    for i in G.nodes:
+    for i in tqdm(G.nodes, desc= "Decay centrality:"):
         centrality[i] = sum(p**l * len(nx.single_source_shortest_path_length(G, i, cutoff=l)) for l in range(1, T+1))
         
     return centrality
@@ -46,11 +47,13 @@ def diffusion_centrality(G, T):
     
     # Compute p^l for l = 1 to T and accumulate the walks
     p_power = np.eye(n)  
-    for l in range(1, T + 1):
+    for l in tqdm(range(1, T + 1), desc="Diffusion centrality:"):
         p_power = np.matmul(p_power, P)  # Compute p^l
         centrality += np.sum(p_power, axis=1)  # Sum over j
+
+    centrality_dict = {node: centrality[i] for i, node in enumerate(G.nodes)}
     
-    return centrality
+    return centrality_dict
 
 def Godfather(G):
     
@@ -64,10 +67,32 @@ def Godfather(G):
     godfather_index = np.zeros(n)
     
     # Compute the Godfather index for each node i
-    for i in range(n):
+    for i in tqdm(range(n), "Godfather index:"):
         for j in range(n):
             for k in range(j + 1, n):
                 if g[k, j] == 0 and g[j, k] == 0:
                     godfather_index[i] += g[k, i] * g[j, i]                    
 
-    return godfather_index
+    godfather_index_dict = {node: godfather_index[i] for i, node in enumerate(G.nodes)}
+
+    return godfather_index_dict
+
+    #TODO: make it less efficient
+    
+# def Godfather(G):
+#     n = len(G.nodes)
+    
+#     # Get the adjacency matrix
+#     g = nx.adjacency_matrix(G).todense()
+    
+#     # Initialize Godfather index array
+#     godfather_index = np.zeros(n)
+    
+#     # Compute the Godfather index for each node i
+#     for i in tqdm(range(n), "Godfather index_corr:"):
+#         # Use broadcasting and vectorized operations
+#         godfather_index[i] = np.sum((g[:, i] * g.T[:, i]) * (1 - g) * (1 - g.T))
+
+#     godfather_index_dict = {node: godfather_index[i] for i, node in enumerate(G.nodes)}
+
+#     return godfather_index_dict
